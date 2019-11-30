c6caae235955f22dfa78086116ad622837bb1175
WidAbid <56513350+WidAbid@users.noreply.github.com>
2019-11-30 13:22:52 +0300

    Wid Abid

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// $( "#clicker" ).click(function() {
+//     $("#secret").fadeIn( "slow", function() {
+//     });
+//   });
+
+
+//   $( "#clicker" ).click(function() {
+//     $( "#secret" ).slideDown( "slow", function() {
+//     });
+//   });
+
+//   $( "button" ).first().click(function() {
+//     $( "#secret" ).first().fadeToggle( "slow", "linear" );
+//   });
+//   $( "button" ).last().click(function() {
+//     $( "#secret" ).last().fadeToggle( "slow", function() {
+//     });
+//   });
+
+// $( "#clicker" ).click(function() {
+//     $( ".secret" ).slideUp( "slow", function() {
+    
+//     });
+//   });
+
+
+$( "#clicker" ).click(function() {
+        $('#img1').attr('src',src="http://rs284.pbsrc.com/albums/ll36/Bigsteve87/Gifs/Liz_Lemon_High_Five.gif~c200");
+    });
+
